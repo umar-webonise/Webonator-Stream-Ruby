40e4e3f9cf5720eaff8fba5776c78b3ed94babe6
Umar Siddiqui <umar.s@weboniselab.com>
2014-12-22 11:41:55 +0530

    Ruby Assignment 5 Meta program added other csv files for review

--- MetaProgramming/vehicle_report.rb
+++ MetaProgramming/vehicle_report.rb
@@ -1,6 +1,6 @@
 # Program to convert CSV file in ruby Class and Onjects
 require 'csv'
-FILE_NAME = 'users.csv'
+FILE_NAME = 'vehicle_report.csv'
 
 file_name = FILE_NAME.gsub(/s?\.csv/, '')
 class_name = file_name.split(/_/).map(&:capitalize)
@@ -15,6 +15,6 @@ CSV.foreach(FILE_NAME, options) do |row|
     csv_class.class_eval { attr_accessor instance_var }
     csv_object.instance_variable_set("@#{instance_var}", instance_vals)
   end
-  puts '-----------------------------------------------------'
+  puts '---------------------------------------------------------------------'
   puts csv_object.inspect
 end

be9832a4abbffef9d78110d2f79fcbf610134d37
Umar Siddiqui <umar.s@weboniselab.com>
2014-12-24 18:07:34 +0530

    put program in a module as CsvToObject converter

--- MetaProgramming/csv_to_objects.rb
+++ MetaProgramming/csv_to_objects.rb
@@ -1,20 +1,24 @@
 # Program to convert CSV file in ruby Class and Onjects
 require 'csv'
-FILE_NAME = 'vehicle_report.csv'
-
-file_name = FILE_NAME.gsub(/s?\.csv/, '')
-class_name = file_name.split(/_/).map(&:capitalize)
-class_name = class_name.join
-
-csv_class = Object.const_set(class_name, Class.new)
-options = { headers: true, header_converters: :symbol, converters: :all }
-CSV.foreach(FILE_NAME, options) do |row|
-  csv_object = csv_class.new
-  row = row.to_hash
-  row.each do |instance_var, instance_vals|
-    csv_class.class_eval { attr_accessor instance_var }
-    csv_object.instance_variable_set("@#{instance_var}", instance_vals)
+# Module for Csv to Object Converter
+module CsvConverterObject
+  def self.convert(file)
+    file_name = file.gsub(/s?\.csv/, '')
+    class_name = file_name.split(/_/).map(&:capitalize)
+    class_name = class_name.join
+    csv_class = Object.const_set(class_name, Class.new)
+    options = { headers: true, header_converters: :symbol, converters: :all }
+    CSV.foreach(file, options) do |row|
+      csv_object = csv_class.new
+      row = row.to_hash
+      row.each do |instance_var, instance_vals|
+        csv_class.class_eval { attr_accessor instance_var }
+        csv_object.instance_variable_set("@#{instance_var}", instance_vals)
+      end
+      puts '---------------------------------------------------------------'
+      puts csv_object.inspect
+    end
   end
-  puts '---------------------------------------------------------------------'
-  puts csv_object.inspect
 end
+
+CsvConverterObject.convert('user_details.csv')
